7fee2e2f8942998cb537d28d8d433c400e1159b2
Exhnil <g.confiac@live.fr>
2025-10-03 03:39:59 +0900

    Forgotten file last commit

--- src/pages/inventory/Inventory.tsx
+++ src/pages/inventory/Inventory.tsx
@@ -6,49 +6,55 @@ import { calculate } from "@/lib/calculateMaterials";
 import { useCharactersStore } from "@/store/CharactersStore";
 import { axiosInstance } from "@/lib/axios";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Button } from "@/components/ui/button";
 
 const STORAGE_KEY = "inventoryState";
 const creditIcon = `${axiosInstance.defaults.baseURL}/materials/shell_credit/item_shell_credit.png`
 
 const Inventory = () => {
   const { items, fetchAllMaterials } = useItemStore();
-
-  const [itemsState, setItemsState] = useState<ItemState[]>([]);
-  const [credits, setCredits] = useState<number>(0)
-  const [requiredCredits, setRequiredCredits] = useState<number>(0)
-
   const { characters, fetchCharacters } = useCharactersStore()
 
+  const [itemsState, setItemsState] = useState<ItemState[]>([]);
+  const [requiredMaterials, setRequiredMaterials] = useState<Record<string, { value: number }>>({});
+  const [showOnlyRequired, setShowOnlyRequired] = useState(false)
+
   useEffect(() => {
-
     const loadData = async () => {
-      await Promise.all([
-        fetchAllMaterials(),
-        fetchCharacters()
-      ]);
-
-      //const saved = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]")
-      const saved: ItemState[] = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]")
-      const itemsStateMap = Object.fromEntries(saved.map(s => [s.id, s]))
-
-      const merged: ItemState[] = items.map((item: Item) => {
-        const state = itemsStateMap[item.id]
-        //const state = saved.find((s: ItemState) => s.id === item.id);
-        return {
-          id: item.id,
-          name: item.name,
-          owned: state ? state.owned : 0,
-          required: state ? state.required : 1
-        }
-      })
-      setItemsState(merged)
+      await Promise.all([fetchAllMaterials(), fetchCharacters])
     }
     loadData()
-  }, [fetchCharacters, items, fetchAllMaterials])
+  }, [fetchAllMaterials, fetchCharacters])
 
   useEffect(() => {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(itemsState))
-  }, [itemsState])
+    if (items.length === 0) return
+
+    let savedItems: ItemState[] = [];
+
+    const savedJson = localStorage.getItem(STORAGE_KEY);
+    savedItems = savedJson ? JSON.parse(savedJson) : [];
+    const itemsStateMap = Object.fromEntries(savedItems.map(s => [s.id, s]));
+
+    const savedCharacters = localStorage.getItem("characterState")
+    const parsed: Record<string, CharacterState> = savedCharacters ? JSON.parse(savedCharacters) : {}
+
+    if (characters.length === 0) return
+
+    const result = calculate(characters, parsed)
+    setRequiredMaterials(result)
+
+
+    const merged: ItemState[] = items.map((item: Item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        owned: itemsStateMap[item.id]?.owned ?? 0,
+        required: result[item.name]?.value ?? 0
+      };
+    });
+
+    setItemsState(merged);
+  }, [characters, items]);
 
   const handleOwnedChange = (id: string, value: number) => {
     setItemsState((prev) =>
@@ -58,23 +64,33 @@ const Inventory = () => {
   }
 
   useEffect(() => {
-    const saved = localStorage.getItem("characterState")
-    const parsed: Record<string, CharacterState> = saved ? JSON.parse(saved) : {}
-    const requiredMaterials = calculate(characters, parsed)
+    const timeout = setTimeout(() => {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(itemsState))
+    }, 300)
 
-    setRequiredCredits(requiredMaterials["Shell Credit"]?.value ?? 0)
+    return () => clearTimeout(timeout)
+  }, [itemsState])
+
+  useEffect(() => {
     setItemsState(prev =>
       prev.map(item => ({
         ...item,
         required: requiredMaterials[item.name]?.value ?? 0
       }))
     )
-  }, [characters])
+  }, [requiredMaterials])
 
   return (
     <div className="p-6">
       <div className="items-center mb-4">
         <h1 className="text-2xl font-bold mb-2">Inventory</h1>
+      </div>
+
+      <div className="flex">
+        <Button
+          className="text-gray-300 bg-gray-600 hover:bg-gray-700"
+          onClick={() => setShowOnlyRequired(prev => !prev)}
+        >{showOnlyRequired ? "Show All" : "Required only"}</Button>
       </div>
 
       <div className="p-2">
@@ -93,36 +109,50 @@ const Inventory = () => {
               </TooltipContent>
             </Tooltip>
           </div>
-          <div className="flex flex-col flex-1">
-            <span
-              className={`text-center px-1 py-0.5 overflow-hidden text-sm font-semibold ${credits >= requiredCredits ? "bg-green-400" : "bg-red-400"}`}
-            >
-              {requiredCredits}
-            </span>
-            <input
-              value={credits}
-              onChange={(e) => setCredits(Number(e.target.value))}
-              className="text-center flex-1 rounded-br px-1 py-0.5 bg-gray-700" />
-          </div>
+          {
+            items
+              .filter(item => item.name === "Shell Credit")
+              .map((item) => {
+                const state = itemsState.find(s => s.id === item.id)
+                if (!state) return null
+                return (
+                  <div key={item.id} className="flex flex-col flex-1">
+
+                    <span
+                      className={`text-center px-1 py-0.5 overflow-hidden text-sm font-semibold ${(state.owned ?? 0) >= (state.required ?? 0) ? "bg-green-400" : "bg-red-400"}`}
+                    >
+                      {state.required}
+                    </span>
+                    <input
+                      value={state.owned ?? 0}
+                      onChange={(e) => handleOwnedChange(item.id, Number(e.target.value))}
+                      className="text-center flex-1 rounded-br px-1 py-0.5 bg-gray-700" />
+                  </div>)
+              })
+          }
         </div>
       </div>
 
       <div className="rounded p-2">
         <div className="grid grid-cols-12 gap-4">
-          {itemsState
-            .filter(state => state.required)
-            .map(state => {
-              const item = items.find((s) => s.id === state.id);
-              if (!item) return null
-
-              return (
-                <InventoryItem
-                  key={item.id}
-                  item={item}
-                  state={state}
-                  onChange={(value) => handleOwnedChange(item.id, value)} />
-              )
-            })}
+          {
+            items
+              .filter((item) => item.name !== "Shell Credit")
+              .map((item) => {
+                const state = itemsState.find(s => s.id === item.id)
+                if (!state) return null
+                if (showOnlyRequired && (state.required ?? 0) <= 0) {
+                  return null
+                }
+                return (
+                  <InventoryItem
+                    key={item.id}
+                    item={item}
+                    state={state}
+                    onChange={(value) => handleOwnedChange(item.id, value)} />
+                )
+              })
+          }
         </div>
       </div>
     </div>
