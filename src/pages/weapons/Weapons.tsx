1b8d80c710270da1609766ccad1361a9d80826ab
Exhnil <g.confiac@live.fr>
2025-10-05 11:19:35 +0900

    error removal, build config

--- src/pages/weapons/Weapons.tsx
+++ src/pages/weapons/Weapons.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Weapons = () => {
   return (
     <div>Weapons</div>
--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build:{
+    outDir:path.resolve(__dirname, "../../api/wuwa-api/public")
+  },
   server: {
     port: 3000,
   },
