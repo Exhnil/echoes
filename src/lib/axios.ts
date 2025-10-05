ec7e74a37d44bf1e2b1392bf0dc4a55ed45e6ca1
Exhnil <g.confiac@live.fr>
2025-10-05 18:41:48 +0900

    added prod base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:5000" : "",
+    import.meta.env.MODE === "development" ? "http://localhost:5000" : "http://91.107.194.37/api",
 });
