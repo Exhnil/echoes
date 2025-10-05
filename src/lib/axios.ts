e3f98cbcff451d757f6f78dfeca3cf4f5d64dc72
Exhnil <g.confiac@live.fr>
2025-10-06 05:00:48 +0900

    .env change

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log(import.meta.env.MODE);
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.VITE_MODE === "development"
+    import.meta.env.MODE === "development"
       ? "http://localhost:3000/api"
       : import.meta.env.VITE_API_URL,
 });
