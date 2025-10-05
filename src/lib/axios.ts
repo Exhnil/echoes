d16a97b70e9996f9fca82a054eb140d2f97d6b08
Exhnil <g.confiac@live.fr>
2025-10-06 04:58:28 +0900

    slight .env gvariable name correction

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log(import.meta.env.MODE);
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development"
+    import.meta.env.VITE_MODE === "development"
       ? "http://localhost:3000/api"
-      : import.meta.env.API_URL,
+      : import.meta.env.VITE_API_URL,
 });
