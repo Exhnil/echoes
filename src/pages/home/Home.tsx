a6eccb7f67d7b8a383cbc7c0ab8f739710b19cf6
exhnil <g.confiac@live.fr>
2026-01-20 17:12:46 +0100

    Added new font, made basic homepage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,21 @@
 const Home = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-full text-center p-6">
-            <h1 className="text-4xl font-bold mb-4">Home</h1>
-            <p className="text-lg text-zinc-400">
-                Coming soon… 🚧
-            </p>
+  return (
+    <div className="flex justify-center px-4 mt-5">
+      <div className="max-w-5xl w-full bg-zinc-700 rounded-2xl shadow-xl p-8">
+        <div className="mb-6 flex justify-center">
+          <img src="/echoes.png" alt="Echoes" className="max-h-48" />
         </div>
-    )
-}
+        <h1 className="text-3xl font-bold mb-4 text-center">
+          Easier Wuthering Waves Farming
+        </h1>
+        <p className="text-zinc-400 leading-relaxed text-center">
+          Welcome to Echoes. With it, you can better organize your farming.
+          Define goals for characters and weapons. Display your inventory and
+          find our where and how much to farm materials.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
